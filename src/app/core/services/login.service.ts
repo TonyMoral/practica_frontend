b3e3a60f46ee7c6e6074a8d95b158b8c0f00f77b
Tony Moral <antonio.moral.correa@gmail.com>
2026-05-11 19:31:39 +0200

    Implementar formulario de inicio de sesión y mejorar el servicio de autenticación

--- src/app/core/services/login.service.ts
+++ src/app/core/services/login.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import to from "./utils.service";
+import ConstUrls from 'src/app/shared/contants/const-urls'; 
 
 
 @Injectable({
@@ -10,10 +11,13 @@ export class LoginService {
   constructor(private http: HttpClient) {}
 
   async iniciarSesion(nickUsuario: string, contrasena: string) {
+    let params = new HttpParams()
+        .set(ConstUrls.NICK_USUARIO_PARAM, nickUsuario)
+        .set(ConstUrls.PASS_USUARIO_PARAM, contrasena);
     return await to(
         this.http
-            .post<boolean>('http://localhost:8080/api/login',
-                null, { params: { nickUsuario: nickUsuario, contrasena: contrasena } })
+            .post<boolean>('http://localhost:8080/usuarios/login',
+                null, { params: params })
             .toPromise()
     )
   }
--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from 'src/app/core/services/login.service';
 import ConstLocalStorage from 'src/app/shared/contants/const-local-storage';
@@ -9,18 +10,35 @@ import ConstLocalStorage from 'src/app/shared/contants/const-local-storage';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
   standalone: true,
-  imports: []
+  imports: [
+     FormsModule
+  ]
 })
 export class LoginComponent {
 
   LoginService: LoginService;
+  nickUsuario: string = '';
+  contrasena: string = '';
 
   constructor(private router: Router, private loginService: LoginService) {
     this.LoginService = loginService;
   }
 
-  public iniciarSesion() {
-    console.log('Iniciar sesión');}
+  public async iniciarSesion() {
+    console.log('Pulsado botón Iniciar sesión');
+    let result = await this.LoginService.iniciarSesion(this.nickUsuario, this.contrasena);
+    if(result === true){
+      console.log('Inicio de sesión exitoso');
+      localStorage.setItem('nickUsuario', this.nickUsuario);
+      localStorage.setItem('contrasena', this.contrasena);
+      this.router.navigate(['/usuarios']);
+    }
+    else{
+      alert('Credenciales incorrectas');
+    }
+console.log('Resultado del inicio de sesión:', result);
+  }
+
 
   // @TODO: Implementar métodos, atributos, etc. necesarios para el funcionamiento del login
 }
